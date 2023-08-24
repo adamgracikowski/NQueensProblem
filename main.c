b019e5dd15ac8d2a1871b95a1463d07019e3b500
Adam <admgrac@gmail.com>
2023-08-24 09:14:13 +0200

    add exemplary usage in main.c

--- main.c
+++ main.c
@@ -2,6 +2,19 @@
 
 int main(void)
 {
+    srand((unsigned)time(NULL));
+
+    const char *sizeMsg = "Enter size of the chessboard: ",
+               *errorMsg = "Invalid input.";
+    int size;
+
+    printf(sizeMsg);
+    if (scanf("%d", &size) != 1)
+    {
+        printf(errorMsg);
+        return -1;
+    }
+    solveNQueenProblem(size);
     system("pause");
     return 0;
 }
